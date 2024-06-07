4344b8cbc9dd22294f1881637061c67e632145b6
Andre Vale <andrevale_6@hotmail.com>
2024-06-07 20:50:41 +0100

    change colors and clean code

--- app/src/main/java/com/example/chamasegura/fragments/fragment_login.kt
+++ app/src/main/java/com/example/chamasegura/fragments/fragment_login.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.chamasegura.R
 
 class fragment_login : Fragment() {
@@ -14,6 +16,15 @@ class fragment_login : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        // Find the button and set an OnClickListener
+        val loginButton: Button = view.findViewById(R.id.login_confirm_button)
+        loginButton.setOnClickListener {
+            // Navigate to the fragment_home
+            findNavController().navigate(R.id.action_fragment_login_to_fragment_home)
+        }
+
+        return view
     }
 }
