b1b5138432d7a8ff03272c173974fe8d30b4715b
Andre Vale <andrevale_6@hotmail.com>
2024-06-08 02:06:26 +0100

    home fragment created

--- app/src/main/java/com/example/chamasegura/fragment_home.kt
+++ app/src/main/java/com/example/chamasegura/fragment_home.kt
@@ -5,11 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+
 class fragment_home : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        // Configurar a toolbar
+        val toolbar = view.findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setNavigationIcon(R.drawable.baseline_menu_24)
+        toolbar.setNavigationOnClickListener {
+            (activity as MainActivity).drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        return view
     }
 }
