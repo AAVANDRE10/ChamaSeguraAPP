7a981f8dc81b981fa738fd139bd6938753db1445
Andre Vale <andrevale_6@hotmail.com>
2024-06-08 02:06:52 +0100

    manage profile fragment created

--- app/src/main/java/com/example/chamasegura/fragments/fragment_manage_profile.kt
+++ app/src/main/java/com/example/chamasegura/fragments/fragment_manage_profile.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import com.example.chamasegura.MainActivity
 import com.example.chamasegura.R
 
 class fragment_manage_profile : Fragment() {
@@ -14,6 +17,15 @@ class fragment_manage_profile : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manage_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_manage_profile, container, false)
+
+        // Configurar a toolbar
+        val toolbar = view.findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setNavigationIcon(R.drawable.baseline_menu_24)
+        toolbar.setNavigationOnClickListener {
+            (activity as MainActivity).drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        return view
     }
 }
